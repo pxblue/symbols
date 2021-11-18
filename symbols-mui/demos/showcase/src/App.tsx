5606b3a0aa36511df4be1a20af5367d45fe4eeab
Suraj Karambe <bkarambe@gmail.com>
2021-11-18 10:42:17 +0530

    PXBLUE-2655 Rebranded symbols mui demo

--- symbols-mui/demos/showcase/src/App.tsx
+++ symbols-mui/demos/showcase/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import * as Symbols from '@pxblue/symbols-mui';
+import * as Symbols from '@brightlayer-ui/symbols-mui';
 
 export const App = (): JSX.Element => (
-    <div>
+    <div data-testid="symbolContainer">
         {Object.keys(Symbols).map((key: any) => {
             //@ts-ignore
             const Component = Symbols[key];
